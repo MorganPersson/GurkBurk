3d369cef83e4d5a9dfd9ae57136e726415988794
Morgan Persson <lazydev@gmail.com>
2012-09-13 12:02:11 +0200

    Fail build with ps1 scripts in teamcity

--- Run-build.ps1
+++ Run-build.ps1
@@ -9,12 +9,18 @@ Write-Host "task $task"
 Write-Host "version $version"
 
 function Build($taskToRun) {
+	Set-Content -path "currentBuildStatus.txt" "yellow"
 	invoke-psake $buildFile -framework '4.0x86' -t $taskToRun -parameters @{"version"="$version"}
-	if ($LastExitCode -ne $null) {
-		if ($LastExitCode -ne 0) {
-			$msg = "build exited with errorcode $LastExitCode"
-			throw $msg
-		}
+	if ($psake.build_success) {
+		Set-Content -path "currentBuildStatus.txt" "passed"
+	}
+	else {
+		Set-Content -path "currentBuildStatus.txt" "failure"
+		$errMsg = $psake.build_error_message
+		$replace = @{ "\|" = "||"; "`n" = "|n"; "`r" = "|r"; "'" = "|'"; "\[" = "|["; "\]"  = "|]"; '0x0085' = "|x"; '0x2028' = "|l"; '0x2029' = "|p" }
+		$replace.GetEnumerator() | ForEach-Object {  $errMsg = $errMsg -replace $_.Key, $_.Value }
+		write-host "##teamcity[message text='build failed: $errMsg' errorDetails='$errMsg' status='ERROR']"
+		throw $errMsg
 	}
 }
 
