a2c92e8513b0f1ef543d074c5bb18a88b5142a48
Morgan Persson <morgan@pinmeto.com>
2018-04-23 08:36:27 +0200

    path fix

--- src/GurkBurk/Properties/AssemblyInfo.cs
+++ src/GurkBurk/Properties/AssemblyInfo.cs
@@ -14,6 +14,6 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("775e74bd-cf82-46c2-9689-d322e0cf86fe")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.1")]
+[assembly: AssemblyFileVersion("2.0.0.1")]
+[assembly: AssemblyInformationalVersion("2.0.0.1")]
